4cb77ec206e4dcfe3fbfe7f7379ee3ef6ac689bf
blazebomb <ashishkumar850601@gmail.com>
2023-11-28 20:24:55 +0530

    first commit

---
+++ contracts/token.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Token is ERC20, Ownable {
+    constructor(address initialOwner) ERC20("latin", "LTN") Ownable(initialOwner) {
+        _mint(msg.sender, 10000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public {
+        _burn(from, amount);
+    }
+}
